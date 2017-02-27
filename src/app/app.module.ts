755de1adfacbcdced1754f8b5baebf881fdc7f6b
Kyle O'Neill <oneillkyle@gmail.com>
2017-02-27 16:44:32 -0500

    Material implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
+import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
+import { MaterialModule } from '@angular/material';
 import { HttpModule }    from '@angular/http';
 // Imports for loading & configuring the in-memory web api
 import { AppComponent }  from './app.component';
@@ -28,6 +29,8 @@ import {PostService} from './posts/post.service';
         FormsModule,
         HttpModule,
         routing,
+        ReactiveFormsModule,
+        MaterialModule,
         AngularFireModule.initializeApp(firebaseConfig, firebaseAuthConfig),
         CKEditorModule,
         DatePickerModule
